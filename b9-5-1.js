96758645d67413c18d57479cdc099706ccda981f
lprmrl <lcthulhu666l@gmail.com>
2022-06-15 14:47:34 +0000

    add file *.js, *.json

---
+++ b9-5-1.js
@@ -0,0 +1,5 @@
+var users = new Array("Bill", "Tom", "Alice");
+var people = ["Sam", "John", "Kate"];
+
+console.log(users); // ["Bill", "Tom", "Alice"]
+console.log(people); // ["John", "Sam", "Kate"]
